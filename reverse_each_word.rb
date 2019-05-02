2366c54dcda587a1f5dfb62d02793c533bf09888
Stephen Brewer <stephenbrewer16@gmial.com>
2019-05-02 12:41:29 -0400

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,6 @@
+# def reverse_each_word(string)
+  # string.split(/ /).each {|word| puts word.reverse!}.join(" ")
+# end
+def reverse_each_word(string)
+  string.split(/ /).collect {|word| word.reverse!}.join(" ")
+end
